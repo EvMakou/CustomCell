5216829cd6b4ef8dbe189b24e9a82f74fd1ecaee
supermacho <face_buk@mail.ru>
2017-10-03 17:45:24 +0300

    Initial Commit

---
+++ CustomCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCells
+//
+//  Created by supermacho on 03.10.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
